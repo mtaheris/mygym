8f035b1327c1330048848d41cbc65acd9a308a96
mtaheris <125962417+mtaheris@users.noreply.github.com>
2024-08-24 11:10:24 +0330

    Update mountain_car.py

--- gym/envs/classic_control/mountain_car.py
+++ gym/envs/classic_control/mountain_car.py
@@ -144,12 +144,12 @@ class MountainCarEnv(gym.Env):
         
         reward = -1.0
         
-        if terminated:
-            reward=100.0
+        #if terminated:
+        #    reward=100.0
             
-        if (position<=-1.2):
-            terminated= True
-            reward=-100.0
+        #if (position<=-1.2):
+        #    terminated= True
+        #    reward=-100.0
         
 
         self.state = (position, velocity)
